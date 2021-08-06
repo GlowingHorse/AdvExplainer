419e8a99534f66967ae3bebc6569b2022bee5bde
R SHI <shirui@g.ecc.u-tokyo.ac.jp>
2021-08-06 17:20:02 +0900

    add

--- src/js/index.js
+++
@@ -1,5 +0,0 @@
-import * as style from "../css/style.css";
-import * as styleHeader from "../css/header.css";
-import * as search from "../css/search.css";
-// import * as post from "../css/post.css";
-import * as ag from './right_graph'
